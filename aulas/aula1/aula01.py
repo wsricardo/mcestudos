0cbdf3fbb7a4ae324b329ef526135787fba7bfcd
Wandeson Ricardo <wandeson.math@gmail.com>
2017-07-18 19:16:52 -0300

    Updates example code aula01.py

---
+++ aulas/aula1/aula01.py
@@ -0,0 +1,16 @@
+#/usr/bin/python3
+
+# Variaveis
+# Número (inteiros, ponto flutuante)
+
+# Suponha que esteja a criar um game
+# e deseje armazenar os pontos de 
+# vida, a altura e o nome de seu personagem.
+# Teriamos.
+HP = 60 # Pontos de vida. (Hit Point)
+S = 1.85 # altura do personagem
+Name = "Aragorn" # Nome do personagem
+
+# HP é uma variavel inteira.
+# S é uma variavel em 'ponto flutuante' (numero do conjunto Real)
+# Name é uma variavel tipo string (sequência de caracteres).
