09ae7ceeb69ee072475eef044347f0bd5a2ff1e8
Wandeson Ricardo <wsricardo@users.noreply.github.com>
2017-08-06 19:27:57 -0300

    Delete test_wsoup.py

--- PythonSamples/test_wsoup.py
+++
@@ -1,9 +0,0 @@
-import wsoup
-import sys
-import os
-
-url_name = sys.argv[1]
-if __name__ == '__main__':
-	cont = wsoup.wSoupCrawler(url_name) # Conteudo html da web em string.
-	text = cont.urlGet()
-	print(text)
